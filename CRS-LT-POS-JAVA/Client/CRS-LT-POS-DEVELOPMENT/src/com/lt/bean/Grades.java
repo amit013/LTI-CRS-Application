f16fc2afedbcadcd1b8f4348a422057029933b6e
Aishwarya <34534078+aishwaryaputhran@users.noreply.github.com>
2021-09-24 12:02:31 +0530

    commit for comments added

--- CRS-LT-POS-JAVA/Client/CRS-LT-POS-DEVELOPMENT/src/com/lt/bean/Grades.java
+++ CRS-LT-POS-JAVA/Client/CRS-LT-POS-DEVELOPMENT/src/com/lt/bean/Grades.java
@@ -11,21 +11,47 @@ public class Grades {
 	private int StudentId;
 	private String Grade;
 	private String CourseId;
+	
+	/**
+	 *Method to get StudentId
+	 * @return studentID
+	 */
 	public int getStudentId() {
 		return StudentId;
 	}
+	/**
+	 *Method to set StudentId
+	 */
 	public void setStudentId(int studentId) {
 		StudentId = studentId;
 	}
+	
+	/**
+	 *Method to get grade
+	 * @return grade
+	 */
 	public String getGrade() {
 		return Grade;
 	}
+	
+	/**
+	 *Method to set grade
+	 */
 	public void setGrade(String grade) {
 		Grade = grade;
 	}
+	
+	/**
+	 *Method to get courseId
+	 * @return courseId
+	 */
 	public String getCourseId() {
 		return CourseId;
 	}
+	
+	/**
+	 *Method to set courseId 
+	 */
 	public void setCourseId(String courseId) {
 		CourseId = courseId;
 	}
